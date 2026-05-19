595ed91d5150ddd7ba072b0d398fbe4c263347a5
Naman <naman@college.com>
2026-05-19 08:14:02 +0530

    resend

--- proctoring-backend/utils/sendEmail.js
+++ proctoring-backend/utils/sendEmail.js
@@ -1,15 +1,9 @@
-const nodemailer = require('nodemailer');
+const { Resend } = require('resend');
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async ({ to, subject, html }) => {
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com', // use hostname, not hardcoded IP
-    port: 465,              // 465 is more reliable than 587 on cloud
-    secure: true,           // true for 465
-    family: 4,              // 👈 forces IPv4, fixes ENETUNREACH
-    auth: { user: process.env.SMTP_USER, pass: process.env.SMTP_PASS },
-  });
-
-  await transporter.sendMail({
+  await resend.emails.send({
     from: process.env.SMTP_FROM,
     to,
     subject,
